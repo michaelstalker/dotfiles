adf50ff366e225f4e29cf998ac5d3865911ab2dc
Michael Stalker <mtstalker@gmail.com>
2016-03-30 09:48:05 -0400

    Configure emacs for tmux

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,26 +18,62 @@
 (quelpa '(use-package :fetcher github :repo "jwiegley/use-package" :stable t))
 (quelpa '(quelpa-use-package :fetcher github :repo "quelpa/quelpa-use-package"))
 (require 'quelpa-use-package)
-
 (setq use-package-always-ensure t)
 (quelpa-use-package-activate-advice)
 
-;(use-package smartparens
-;  :config
-;  (sp-use-smartparens-bindings)
-;  (show-smartparens-global-mode)
-;  (smartparens-global-strict-mode)
-;  (require 'smartparens-config)
-;  (set-face-background 'sp-show-pair-match-face "deep sky blue")
-;  (set-face-foreground 'sp-show-pair-match-face "white"))
+;; Parens
+(use-package smartparens
+  :config
+  (sp-use-smartparens-bindings)
+  (show-smartparens-global-mode)
+  (smartparens-global-strict-mode)
+  (require 'smartparens-config)
+  (set-face-background 'sp-show-pair-match-face "deep sky blue")
+  (set-face-foreground 'sp-show-pair-match-face "white"))
 
-;; Projectilef project search
+(defadvice terminal-init-screen
+    ;; The advice is named `tmux', and is run before `terminal-init-screen' runs.
+    (before tmux activate)
+  ;; Docstring.  This describes the advice and is made available inside emacs;
+  ;; for example when doing C-h f terminal-init-screen RET
+  "Apply xterm keymap, allowing use of keys passed through tmux."
+  ;; This is the elisp code that is run before `terminal-init-screen'.
+  (if (getenv "TMUX")
+      (let ((map (copy-keymap xterm-function-map)))
+	(set-keymap-parent map (keymap-parent input-decode-map))
+	(set-keymap-parent input-decode-map map))))
+
+;; Allow certain key combinations in the terminal version of Emacs
+;; (define-key input-decode-map "\e[1;4A" [M-up])
+;; (define-key input-decode-map "\e[1;4B" [M-down])
+;; (define-key input-decode-map "\e[1;4C" [M-right])
+;; (define-key input-decode-map "\e[1;4D" [M-left])
+;; (define-key input-decode-map "\e[1;5A" [C-up])
+;; (define-key input-decode-map "\e[1;5B" [C-down])
+;; (define-key input-decode-map "\e[1;5C" [C-right])
+;; (define-key input-decode-map "\e[1;5D" [C-left])
+;; (define-key input-decode-map "\e[1;8A" [C-M-up])
+;; (define-key input-decode-map "\e[1;8B" [C-M-down])
+;; (define-key input-decode-map "\e[1;8C" [C-M-right])
+;; (define-key input-decode-map "\e[1;8D" [C-M-left])
+
+;; (define-key input-decode-map "\e[1;8x]" [C-M-x])
+
+(require 'rainbow-delimiters)
+(show-paren-mode 1)
+
+(defun customize-clojure-mode ()
+  (turn-on-smartparens-strict-mode)
+  (rainbow-delimiters-mode))
+
+(add-hook 'clojure-mode-hook 'customize-clojure-mode)
+
+;; Projectile project search
 (projectile-global-mode)
 
 ;; Strip whitespace
-(setq whitespace-action '(auto-cleanup))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Tab 2 spaces in CoffeeScript files
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -60,34 +96,16 @@
 	     (column-number-mode)
 	     (global-linum-mode))
 
-;; Parens
-(require 'rainbow-delimiters)
-(require 'paredit)
-
-(defun customize-clojure-mode ()
-  (paredit-mode 1)
-  (local-set-key "\M-[1;2d" 'paredit-backward-slurp-sexp)
-  (local-set-key "\M-[\ c" 'paredit-forward-slurp-sexp))
-
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'cider-repl-mode-hook #'paredit-mode)
-(add-hook 'clojure-mode-hook 'customize-clojure-mode)
-(show-paren-mode 1)
-;(define-key paredit-mode-map (kbd "M-[") nil)
-
-;; Share OS X clipboard
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-cnnection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+(eval-after-load 'clojure-mode
+  '(define-clojure-indent
+     (defroutes 'defun)
+     (context 'defun)
+     (GET 'defun)
+     (POST 'defun)
+     (PUT 'defun)
+     (DELETE 'defun)
+     (HEAD 'defun)
+     (ANY 'defun)))
 
 (setq slime-contribs '(slime-fancy))
 (setq geiser-active-implementations '(racket))
@@ -97,3 +115,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq
+ backup-by-copying t ; don't clobber symlinks
+ backup-directory-alist `(("." . "~/tmp")) ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t ; use versioned backups
+ auto-save-file-name-transforms `((".*" "~/tmp" t)))
+
+;; IDO
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; enable fuzzy matching
